1b49b048902e82517ab8cc937c1d0589a7cce79d
USMC1941 <16388993+USMC1941@users.noreply.github.com>
2021-08-31 23:00:33 -0400

    Convert RUNUBusRoutes Android project.

---
+++ Code/2017-04-06/RUNBBusRoutes/app/src/main/java/com/example/sesh/runbbusroutes/ShowRoute.java
@@ -0,0 +1,26 @@
+package com.example.sesh.runbbusroutes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowRoute extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.route_detail);
+
+        // get the name and detail from bundle
+        Bundle bundle = getIntent().getExtras();
+        String routeName = bundle.getString(Routes.ROUTE_NAME_KEY);
+        String routeDetail = bundle.getString(Routes.ROUTE_DETAIL_KEY);
+
+        // get the name and detail view objects
+        TextView routeNameView = findViewById(R.id.route_name);
+        TextView routeDetailView = findViewById(R.id.route_detail);
+
+        // set name and detail on the views
+        routeNameView.setText(routeName);
+        routeDetailView.setText(routeDetail);
+    }
+}
