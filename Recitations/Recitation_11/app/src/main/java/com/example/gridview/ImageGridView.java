72011d02cd6e78ad5196f7b5838a8040740dad8f
USMC1941 <16388993+USMC1941@users.noreply.github.com>
2021-09-04 23:23:58 -0400

    Remove unneeded casts.

--- Recitations/Recitation_11/app/src/main/java/com/example/gridview/ImageGridView.java
+++ Recitations/Recitation_11/app/src/main/java/com/example/gridview/ImageGridView.java
@@ -3,7 +3,6 @@ package com.example.gridview;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -19,13 +18,13 @@ public class ImageGridView extends AppCompatActivity {
         setContentView(R.layout.grid_view);
 
         myImgAdapter = new ImageAdapter(this);
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+        GridView gridview = findViewById(R.id.gridview);
         gridview.setAdapter(myImgAdapter);
 
         gridview.setOnItemClickListener((parent, v, position, id) -> {
             LayoutInflater inflater = getLayoutInflater();
             View view = inflater.inflate(R.layout.toast_layout,
-                    (ViewGroup) findViewById(R.id.relativeLayout1));
+                    findViewById(R.id.relativeLayout1));
             view.setBackgroundResource(myImgAdapter.getImgID(position));
 
             Toast toast = new Toast(parent.getContext());
